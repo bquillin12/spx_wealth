8bc462e20f2b0b05040a35d417388be9ff4a1726
Bryce Quillin <bquillin@yahoo.com>
2020-03-03 09:52:53 -0500

    Update spx_wealth_path.R

--- spx_wealth_path.R
+++ spx_wealth_path.R
@@ -12,18 +12,22 @@ library(dygraphs)
 # load data
 sp500_fred <- na.omit(getSymbols("SP500", src = "FRED", auto.assign = FALSE, method = "curl")) 
 
+sp500 <- na.omit(getSymbols("SPY", src = "yahoo", auto.assign = FALSE, method = "curl"))
+
 # calculate daily returns 
-sp500_fred.r <- dailyReturn(sp500_fred)
+sp500_d <- dailyReturn(sp500)
 
 # subset for 2016 returns as we want this year's returns
-sp500.subset <- sp500_fred.r["2016-01-01/2016-06-09"] 
+sp500_d.subset <- sp500_d["2020-01-01/2020-03-02"] 
 
 # draw wealth path
-cumret <- cumprod(1+sp500.subset)
+cumret <- cumprod(1+sp500_d.subset)
 
 # Plot with dygraph
-autoplot.zoo(cumret) +
-  ggtitle("S&P 500 Wealth Path (Jan 1 2016 = $1.00)") +
+autoplot.zoo(cumret, color = "red") +
+  ggtitle("S&P 500 Wealth Path (Jan 1 2020 = $1.00)") +
   ylab("") +
-  xlab("")
-
+  xlab("") +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  theme(plot.title = element_text(hjust = 0.5))
